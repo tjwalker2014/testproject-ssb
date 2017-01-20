b47d43a85403d1c746200fb048ef0aa05426c872
Tom Walker <tj.walker@live.com>
2017-01-20 15:52:58 +0000

    experimenting with sendfile line

--- server/app.js
+++ server/app.js
@@ -26,7 +26,8 @@ var pathToUse;
 app.use(express.static(path.join(__dirname, pathToUse)));
 
 app.get("*", function(req, res) {
-  res.sendFile("index.html", {root: path.join(__dirname, pathToUse)});
+  // res.sendFile("index.html", {root: path.join(__dirname, pathToUse)});
+  res.sendFile("public/dist/index.html", {root: __dirname});
 })
 
 // catch 404 and forward to error handler
